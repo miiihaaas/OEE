7a07b8402fc9c8b0b8f1c0e3d968846e2bdddce3
miiihaaas <miiihaaas@gmail.com>
2021-11-20 16:36:29 +0100

    commit04

--- Actions.py
+++ Actions.py
@@ -1,93 +1,16 @@
-class Actions:
-    def __init__(self):
-        self.NewRecord = "novi zapis"
+import cv2
+from pyzbar.pyzbar import decode
 
+img = cv2.imread("D:\Mihas\Programming\Python\Projects\OEE\OEE\QR_code.png")
+print(decode(img))
 
-    def extrudion(self, end_date_time, id, rn, quantity, worker_id):
-        #id, rn, quantity, end_date_time
-        self.end_date_time = end_date_time
-        self.id = id
-        self.rn = rn
-        self.activity = "ekstrudiranje"
-        self.quantity = quantity
-        self.equipment = "ZSK43"        #treba da je dinaično na osnovu odabrane opreme
-        self.worker_id = worker_id
+for code in decode(img):
+    # print(code.type)
+    # print(code.data.decode("UTF8"))
+    pass
 
-
-
-    def cleaning(self, end_date_time, id, rn, worker_id):
-        self.end_date_time = end_date_time
-        self.id = id
-        self.rn = rn
-        self.activity = "čišćenje opreme"
-        self.quantity = None
-        self.equipment = "ZSK43"        #treba da je dinaično na osnovu odabrane opreme
-        self.worker_id = worker_id
-
-
-    def cleaning_work_space(self, end_date_time, id, rn, worker_id):
-        self.end_date_time = end_date_time
-        self.id = id
-        self.rn = rn
-        self.activity = "čišćenje radnog mesta"
-        self.quantity = None
-        self.equipment = "ZSK43"        #treba da je dinaično na osnovu odabrane opreme
-        self.worker_id = worker_id
-
-
-    def probe_from_mill(self, end_date_time, id, rn, worker_id):
-        self.end_date_time = end_date_time
-        self.id = id
-        self.rn = rn
-        self.activity = "čekanje probe sa mlina"
-        self.quantity = None
-        self.equipment = "ZSK43"        #treba da je dinaično na osnovu odabrane opreme
-        self.worker_id = worker_id
-
-
-    def quality_control(self, end_date_time, id, rn, worker_id):
-        self.end_date_time = end_date_time
-        self.id = id
-        self.rn = rn
-        self.activity = "kontrola"
-        self.quantity = None
-        self.equipment = "ZSK43"        #treba da je dinaično na osnovu odabrane opreme
-        self.worker_id = worker_id
-
-
-    def brakedown(self, end_date_time, id, rn, worker_id):
-        self.end_date_time = end_date_time
-        self.id = id
-        self.rn = rn
-        self.activity = "kvar"
-        self.quantity = None
-        self.equipment = "ZSK43"        #treba da je dinaično na osnovu odabrane opreme
-        self.worker_id = worker_id
-        #dodati ID kvarova
-
-    def no_premix(self, end_date_time, id, rn, worker_id):
-        self.end_date_time = end_date_time
-        self.id = id
-        self.rn = rn
-        self.activity = "nema premiksa"
-        self.quantity = None
-        self.equipment = "ZSK43"        #treba da je dinaično na osnovu odabrane opreme
-        self.worker_id = worker_id
-
-    def warm_up(self, end_date_time, id, rn, worker_id):
-        self.end_date_time = end_date_time
-        self.id = id
-        self.rn = rn
-        self.activity = "zagrevanje"
-        self.quantity = None
-        self.equipment = "ZSK43"        #treba da je dinaično na osnovu odabrane opreme
-        self.worker_id = worker_id
-
-    def no_operator(self, end_date_time, id, rn, worker_id):
-        self.end_date_time = end_date_time
-        self.id = id
-        self.rn = rn
-        self.activity = "nema operatora"
-        self.quantity = None
-        self.equipment = "ZSK43"        #treba da je dinaično na osnovu odabrane opreme
-        self.worker_id = worker_id
+txt = code.data.decode("UTF8")
+x = txt.split("-")
+id_, rn = x[0], x[1]
+print(f"ID gotovog proizvoda je: {id_}")
+print(f"Radni nalog gotovog proizvoda je: {rn}")
--- NewRecord.py
+++ NewRecord.py
@@ -1,6 +1,8 @@
-class NewRecord:
+import datetime
+
+class New_Record:
     #constructor
-    def __init__(self, end_date_time, id, activity, quantity, equipment, worker_id):
+    def __init__(self, end_date_time, id, activity, quantity, equipment, worker_id=5):
         self.end_date_time = end_date_time
         self.id = id
         self.rn = rn
@@ -8,3 +10,37 @@ class NewRecord:
         self.quantity = quantity
         self.equipment = equipment
         self.worker_id = worker_id
+
+
+class Extruder(New_Record):
+    def __init__(self, equipment = "ZSK43"):
+        self.equipment = equipment
+
+end_date_time = datetime.datetime.now()
+id = "42100406"
+rn = "1234567"
+# moguće aktivnosti:    "ekstrudiranje", "čišćenje opreme", "čišćenje radnog mesta",
+#                       "čekanje probe sa mlina", "kontrola", "kvar", "nema premiksa",
+#                       "zagrevanje", "nema operatera"
+activity = "nema operatera" # ovo treba da je promenjiva koja se sama menja na osnovu odabrane aktivnosti
+quantity = 260
+equipment = "ZSK43" #treba da je dinaično na osnovu odabrane opreme
+worker_id = "2"
+
+new_ = New_Record(end_date_time, id, activity, quantity, equipment, worker_id)
+print(new_.end_date_time)
+print(new_.id)
+print(new_.rn)
+print(new_.quantity)
+print(new_.equipment)
+print(new_.activity)
+print(new_.worker_id)
+
+new_ = Extruder(equipment)
+print(new_.end_date_time)
+print(new_.id)
+print(new_.rn)
+print(new_.quantity)
+print(new_.equipment)
+print(new_.activity)
+print(new_.worker_id)
