62f1d1ea03798a43d520021a5f7f7b4248800ccc
miiihaaas <miiihaaas@gmail.com>
2021-11-20 23:55:45 +0100

    Commit07

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # OEE
 Overall Efficiency of Equipment
+
+# izgled aplikacije
+1 = ekstrudiranje
+    11 = ZSK43
+        110 = ekstrudiranje
+        111 = čišćenje
+        112 = čišćenje radnog mesta
+        113 = čekanje probe sa mlina
+        114 = kontrola
+        115 = kvar
+        116 = nema premiksa
+        117 = nema konejnera za čips
+        118 = zagrevanje
+        119 = nema operatera
+    12 = APV
+        120 = ekstrudiranje
+        121 = čišćenje
+        122 = čišćenje radnog mesta
+        123 = čekanje probe sa mlina
+        124 = kontrola
+        125 = kvar
+        126 = nema premiksa
+        127 = nema konejnera za čips
+        128 = zagrevanje
+        129 = nema operatera
+2 = mlevenje
+    21 = ACM20
+      210 = mlevenje
+      211 = čišćenje
+      212 = čišćenje radnog mesta
+      213 = kontrola
+      214 = kvar
+      215 = nema čipsa
+      216 = pražnjenje filtera
+      217 = nema operatera
+    22 = ACM30
+      220 = mlevenje
+      221 = čišćenje
+      222 = čišćenje radnog mesta
+      223 = kontrola
+      224 = kvar
+      225 = nema čipsa
+      226 = pražnjenje filtera
+      227 = nema operatera
+
+# aktivirano dugme (npr: 112 = čišćenje radnog mesta) treba da vrati listu sa svim promenjivim
+# u app fajlu treba da se dodeli ta lista new_record koja se dalje append u csv fajl
+
+# input podataka se vrši preko QR koda (pokušaj u fajlu actions.py)
--- app.py
+++ app.py
@@ -3,37 +3,35 @@ import csv
 
 
 # definisane variable sa defoult vrednostima 0
+file_path = "D:\Mihas\Programming\Python\Projects\OEE\OEE\\"
+file_name = "CollectedData.csv"
 end_date_time = 0
 id_ = 0
 rn = 0
-activity = 0
 quantity = 0
 equipment = 0
+activity = 0
 worker_id = 0
 
-new_record = [end_date_time, id_, rn, activity, quantity, equipment, worker_id]
-print(new_record)
 
 #hardkord inputi, treba napraviti funkcije za imput od strane operatera: QR kod, brojevi itd
 end_date_time = datetime.datetime.now()
-print(end_date_time)
 #pretvara ga u string koji excel može da poročita kao datum vreme
 end_date_time = end_date_time.strftime("%x %X")
-print(end_date_time)
 id_ = "42509406"
 rn = "1903425"
 # moguće aktivnosti:    "ekstrudiranje", "čišćenje opreme", "čišćenje radnog mesta",
 #                       "čekanje probe sa mlina", "kontrola", "kvar", "nema premiksa",
 #                       "zagrebanje", "nema operatera"
-activity = "ekstrudiranje" # ovo treba da je promenjiva koja se sama menja na osnovu odabrane aktivnosti
-quantity = 520
+activity = "čišćenje radnog mesta" # ovo treba da je promenjiva koja se sama menja na osnovu odabrane aktivnosti
+quantity = 260
 equipment = "ZSK43" #treba da je dinaično na osnovu odabrane opreme
 worker_id = "5" #id radnika je od 2 pa do 26 - stim što može da se dodaje borj kako se budu zapošljavali novi radnici
 
-new_record = [end_date_time, id_, rn, activity, quantity, equipment, worker_id]
+new_record = [end_date_time, id_, rn, quantity, equipment, activity, worker_id]
 print(new_record)
 
 #append new_record to CollectedData
-with open("D:\Mihas\Programming\Python\Projects\OEE\OEE\CollectedData.csv", "a", encoding='UTF8', newline="") as f:
+with open(file_path + file_name, "a", encoding='UTF8', newline="") as f:
     writer = csv.writer(f)
     writer.writerow(new_record)
