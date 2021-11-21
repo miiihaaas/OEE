f36c8640728714721473aafcbb12aa621746c9d5
miiihaaas <miiihaaas@gmail.com>
2021-11-21 12:16:34 +0100

    commit10

--- app.py
+++ app.py
@@ -46,100 +46,100 @@ menu_tree = {1: "ekstrudiranje",
             226: "pražnjenje filtera",
             227: "nema operatera"
       }
-
-print("1 = ekstrudiranje")
-print("2 = mlevenje")
-choose = input(f"Izabei zonu: ")
-if choose == "1":
+while True:
     print("1 = ekstrudiranje")
-    print(" 11 = ZSK43")
-    print(" 12 = APV")
-    choose = input(f"Izabei ekstruder: ")
-if choose == "11":
-    print("1 = ekstrudiranje")
-    print(" 11 = ZSK43")
-    for i in range (110,120):
-        print(f"     {i} = {menu_tree[i]}")
-        i += 1
-    choose = input(f"Izabei aktivnost: ")
-if choose == "12":
-    print("1 = ekstrudiranje")
-    for i in range (120,130):
-        print(f"     {i} = {menu_tree[i]}")
-        i += 1
-    choose = input(f"Izabei aktivnost: ")
-if choose == "2":
     print("2 = mlevenje")
-    print(" 21 = ACM20")
-    print(" 22 = ACM30")
-    choose = input(f"Izabei mlin: ")
-if choose == "21":
-    print("2 = mlevenje")
-    print(" 21 = ACM20")
-    for i in range (210,220):
-        print(f"     {i} = {menu_tree[i]}")
-        i += 1
-    choose = input(f"Izabei aktivnost: ")
-if choose == "22":
-    print("2 = mlevenje")
-    print(" 22 = ACM20")
-    for i in range (220,230):
-        print(f"     {i} = {menu_tree[i]}")
-        i += 1
-    choose = input(f"Izabei aktivnost: ")
+    choose = input(f"Izabei zonu: ")
+    if choose == "1":
+        print("1 = ekstrudiranje")
+        print(" 11 = ZSK43")
+        print(" 12 = APV")
+        choose = input(f"Izabei ekstruder: ")
+    if choose == "11":
+        print("1 = ekstrudiranje")
+        print(" 11 = ZSK43")
+        for i in range (110,120):
+            print(f"     {i} = {menu_tree[i]}")
+            i += 1
+        choose = input(f"Izabei aktivnost: ")
+    if choose == "12":
+        print("1 = ekstrudiranje")
+        for i in range (120,130):
+            print(f"     {i} = {menu_tree[i]}")
+            i += 1
+        choose = input(f"Izabei aktivnost: ")
+    if choose == "2":
+        print("2 = mlevenje")
+        print(" 21 = ACM20")
+        print(" 22 = ACM30")
+        choose = input(f"Izabei mlin: ")
+    if choose == "21":
+        print("2 = mlevenje")
+        print(" 21 = ACM20")
+        for i in range (210,220):
+            print(f"     {i} = {menu_tree[i]}")
+            i += 1
+        choose = input(f"Izabei aktivnost: ")
+    if choose == "22":
+        print("2 = mlevenje")
+        print(" 22 = ACM20")
+        for i in range (220,230):
+            print(f"     {i} = {menu_tree[i]}")
+            i += 1
+        choose = input(f"Izabei aktivnost: ")
+
+    if int(choose) < 1:
+        break
+
+    file_path = "D:\Mihas\Programming\Python\Projects\OEE\OEE\CollectedData\\"
+
+    # po defoultu je količina 0
+    quantity = 0
+
+    # bira opremu na osnovu odabrane aktivnosti
+    if choose[:2] == "11":
+        equipment = "ZSK43"
+    elif choose[:2] == "12":
+        equipment = "APV"
+    elif choose[:2] == "21":
+        equipment = "ACM20"
+    elif choose[:2] == "22":
+        equipment = "ACM30"
+
+    # definiše ime fajla
+    file_name = f"{equipment}.csv"
+
+    print(f"odabrana oprema: {equipment} \\ {menu_tree[int(choose)]}")
+
+    # input id, rn...
+    id_ = input("unesi ID materijala: ")
+    rn = input("unesi broj RN: ")
+
+    # ako je mlevenje ili ektrudiranje, traži input za količinu
+    if choose[:1] == "1":
+        atribut_ = "Ekstrudirana"
+    elif choose[:1] == "2":
+        atribut_ = "Samlevena"
 
 
-quantity = 0
+    if choose[-1:] == "0":
+        quantity = input(f"{atribut_} količina: ")
 
 
-if choose[:2] == "11":
-    equipment = "ZSK43"
-elif choose[:2] == "12":
-    equipment = "APV"
-elif choose[:2] == "21":
-    equipment = "ACM20"
-elif choose[:2] == "22":
-    equipment = "ACM30"
+    # input aktivnost, operater...
+    activity = menu_tree[int(choose)]
+    worker_id = input("unesi ID operatera: ")
+
+    # trentno vreme
+    end_date_time = datetime.datetime.now()
+    # pretvara ga u string koji excel može da poročita kao datum vreme
+    end_date_time = end_date_time.strftime("%x %X")
 
 
-file_name = f"{equipment}.csv"
-print(equipment)
-print(menu_tree[int(choose)])
+    new_record = [end_date_time, id_, rn, quantity, equipment, activity, worker_id]
+    print(new_record)
 
-id_ = input("unesi ID materijala: ")
-rn = input("unesi broj RN: ")
-
-if choose[:1] == "1":
-    atribut_ = "Ekstrudirana"
-elif choose[:1] == "2":
-    atribut_ = "Samlevena"
-
-
-if choose[-1:] == "0":
-    quantity = input(f"{atribut_} količina: ")
-
-
-
-activity = menu_tree[int(choose)]
-worker_id = input("unesi ID operatera: ")
-
-
-
-
-# definisane variable sa defoult vrednostima 0
-file_path = "D:\Mihas\Programming\Python\Projects\OEE\OEE\CollectedData\\"
-#file_name = "CollectedData.csv"
-
-
-end_date_time = datetime.datetime.now()
-#pretvara ga u string koji excel može da poročita kao datum vreme
-end_date_time = end_date_time.strftime("%x %X")
-
-
-new_record = [end_date_time, id_, rn, quantity, equipment, activity, worker_id]
-print(new_record)
-
-#append new_record to CollectedData
-with open(file_path + file_name, "a", encoding='UTF8', newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(new_record)
+    #append new_record to CollectedData
+    with open(file_path + file_name, "a", encoding='UTF8', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(new_record)
