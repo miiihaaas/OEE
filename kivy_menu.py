21d32407d32455ddb308fc4ffa96eb34b97ad9fa
miiihaaas <miiihaaas@gmail.com>
2021-12-22 19:18:44 +0100

    commit14

--- kivy_menu.py
+++ kivy_menu.py
@@ -1,5 +1,6 @@
 import datetime
 import csv
+from qr_scanner import scan
 
 import kivy
 from kivy.app import App
@@ -11,55 +12,111 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
-from kivy.properties import ListProperty
+from kivy.core.window import Window
 
 
-new_record = ["nedefinisano vreme", "nedefinisan ID", "nedefinisan RN", 0, "nedefinisana oprema", "nedefinisana aktivnost", "nedefinisan radnik"]
+new_record = ["0- ndf vreme", "1 -ndf ID", "2 -ndf RN", "3- ndf slovo", "4- ndf šarža", "5- ndf naziv", 0, "7- ndf oprema!", "8- ndf aktivnost", "9- ndf radnik", "10- ndf pomoćnik", "11- ndf komentar", "12- ndf ID kvara"]
 file_path = "D:\Mihas\Programming\Python\Projects\OEE\OEE\CollectedData\\"
+# file_path = "https://helgrp-my.sharepoint.com/:f:/r/personal/mihailo_panic_helios_rs/Documents/01%20Proizvodnja/01%20OEE/Python/"
 file_name = ""
 
 class Potvrdi():
+    Window.size = (360, 640) # povećati na 720, 1280 kada se bude instaliralo na android
     id_ = ObjectProperty(None)
     rn = ObjectProperty(None)
+    slovo = ObjectProperty(None) #
+    sarza = ObjectProperty(None) #
+    naziv = ObjectProperty(None) #
     quantity = ObjectProperty(None)
     worker_id = ObjectProperty(None)
-    equipment = ObjectProperty(None)
-    activity = "nedefinisana aktivnost"
+    worker_id_pomocnik = ObjectProperty(None) #
+    komentar = ObjectProperty(None) #
+    kvar_id = ObjectProperty(None) #
+    equipment = ObjectProperty(None) #
+    activity = ObjectProperty(None) #
+    last_input = ObjectProperty(None)
+
+
+    def update_equipment_labels(self):
+        self.equipment.text = new_record[7]
+
+
+    def update_activity_labels(self):
+        self.activity.text = new_record[7] + " / " + new_record[8]
+        self.last_input.text = f"Vreme: {new_record[0]}, \nID-RN: {new_record[1]} - {new_record[2]}, \nOprema - Radnik: {new_record[7]} - {new_record[8]}"
 
 
     def btn_eqpmnt_press(self):
-        new_record[4] = self.equipment
+        new_record[7] = self.equipment.text
         print(new_record)
 
+
     def btn_activity_press(self):
-        new_record[5] = self.activity
+        new_record[8] = self.activity.text
         print(new_record)
 
 
     def btn_press(self):
-        new_record[1] = self.id_.text
-        new_record[2] = self.rn.text
-        if new_record[5] == ("Ekstrudiranje" or "Mlevenje"):
-            new_record[3] = self.quantity.text
+        if new_record[8] == "Ekstrudiranje":
+            new_record[6] = self.quantity.text
+        elif new_record[8] == "Mlevenje":
+            new_record[6] = self.quantity.text
         else:
-            new_record[3] = 0
-        new_record[6] = self.worker_id.text
+            new_record[6] = 0
+        new_record[9] = self.worker_id.text
+        new_record[10] = self.worker_id_pomocnik.text
+        new_record[11] = self.komentar.text
+        if new_record[8] == "Kvar":
+            new_record[12] = self.kvar_id.text
+        else:
+            new_record[12] = ""
+
         # trentno vreme
         new_record[0] = datetime.datetime.now()
         # pretvara ga u string koji excel može da poročita kao datum vreme
         new_record[0] = new_record[0].strftime("%x %X")
         print(new_record)
-        file_name = f"{new_record[4]}.csv"
+        file_name = f"{new_record[7]}.csv"
         print(file_path + file_name)
+        # append new_record to .csv file
         with open(file_path + file_name, "a", encoding='UTF8', newline="") as f:
             writer = csv.writer(f)
             writer.writerow(new_record)
-        self.id_.text = ""
-        self.rn.text = ""
-        if new_record[5] == ("Ekstrudiranje" or "Mlevenje"):
+        # reset input values
+        self.id_.text = "ID: "
+        self.rn.text = "RN: "
+        self.slovo.text = "??"
+        self.sarza.text = "??"
+        self.naziv.text = "Naziv: Skeniraj QR Kod"
+        if new_record[8] == "Ekstrudiranje":
+            self.quantity.text = ""
+        elif new_record[8] == "Mlevenje":
             self.quantity.text = ""
         self.worker_id.text = ""
+        self.worker_id_pomocnik.text = ""
+        self.komentar.text = ""
+        self.last_input.text = f"Vreme: {new_record[0]}, \nID-RN: {new_record[1]} - {new_record[2]}, \nAktivnost - Količina: {new_record[7]} - {new_record[5]}kg, \nOprema - Radnik: {new_record[6]} - {new_record[8]}"
 
+
+    def btn_scan(self):
+        id_, rn, slovo, sarza, naziv = scan()
+        print(f"{id_=}, {rn=}, {slovo=}, {sarza=}, {naziv=}")
+        new_record[1] = id_
+        new_record[2] = rn
+        new_record[3] = slovo
+        new_record[4] = sarza
+        new_record[5] = naziv
+        print(new_record)
+        self.id_.text = id_
+        self.rn.text = rn
+        self.slovo.text = f"({slovo})"
+        self.sarza.text = sarza
+        self.naziv.text = naziv
+        pass
+
+
+class AktivnostUniverzalnaWindow(Screen, Potvrdi):
+    pass
 
 
 class MainWindow(Screen):
@@ -77,22 +134,8 @@ class EkstrudiranjeWindow(Screen, Potvrdi):
 class EkstruderAktivnostiWindow(Screen, Potvrdi):
     pass
 
+
 class AktivnostEktrurdiranjeWindow(Screen, Potvrdi):
-    pass
-
-class AktivnostCiscenjeWindow(Screen, Potvrdi):
-    pass
-
-
-class AktivnostCiscenjeRadnogMestaWindow(Screen, Potvrdi):
-    pass
-
-
-class AktivnostCekanjeProbeSaMlinaWindow(Screen, Potvrdi):
-    pass
-
-
-class AktivnostKontrolaWindow(Screen, Potvrdi):
     pass
 
 
@@ -100,35 +143,24 @@ class AktivnostKvarWindow(Screen, Potvrdi):
     pass
 
 
-class AktivnostNemaPremiksaWindow(Screen, Potvrdi):
+class MlevenjeWindow(Screen, Potvrdi):
     pass
 
 
-class AktivnostZagrevanjeWindow(Screen, Potvrdi):
+class MlinAktivnostiWindow(Screen, Potvrdi):
     pass
 
 
-class AktivnostNemaOperateraWindow(Screen, Potvrdi):
-    pass
-
-
-
-
-class MlevenjeWindow(Screen):
-    equipment = "nedefinisana oprema sa mlina"
-    def btn_press(self):
-        new_record[4] = self.equipment
-        print(new_record)
-    pass
-
-class MlinAktivnostiWindow(Screen):
+class AktivnostMlevenjeWindow(Screen, Potvrdi):
     pass
 
 
 class WindowManager(ScreenManager):
     pass
 
+
 kv = Builder.load_file("my.kv")
+
 
 class MyMainApp(App):
     def build(self):
--- qr_scanner.py
+++ qr_scanner.py
@@ -1,36 +1,63 @@
+# link: https://www.youtube.com/watch?v=IOhZqmSrjlE&ab_channel=Pythonenthusiast
+
 import cv2
 from pyzbar.pyzbar import decode
 import time
 
-#####################################
-### ovaj kod čita QR kod sa fajla ###
-#####################################
-img = cv2.imread("D:\Mihas\Programming\Python\Projects\OEE\OEE\QR_code.png")
-print(decode(img))
-
-for code in decode(img):
-    # print(code.type)
-    # print(code.data.decode("UTF8"))
-    pass
-
-txt = code.data.decode("UTF8")
-x = txt.split("-")
-id_, rn = x[0], x[1]
-print(f"ID gotovog proizvoda je: {id_}")
-print(f"Radni nalog gotovog proizvoda je: {rn}")
-time.sleep(3)
 ######################################
 ### ovaj kod čita QR kod sa kamere ###
 ######################################
-cap = cv2.VideoCapture(0)
-cap.set(3, 640) #3 - width
-cap.set(4, 480) #4 - height
+def scan():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640) #3 - width
+    cap.set(4, 480) #4 - height
 
-camera = True
-while camera == True:
-    success, frame = cap.read()
-    print(f"uspešnost: {success}, frejm: {frame}")
-    for code in decode(frame):
-        print(code.data.decode("UTF8"))
-    cv2.imshow("Testing code scan", frame)
-    cv2.waitKey(1)
+    id_, rn, slovo, sarza, naziv = 0, 0, 0, 0, 0
+    print (id_, rn, slovo, sarza, naziv)
+
+    camera = True # False by default ali kada se prtisne dugme za skenjranje, treba da promeni u True
+    while camera == True:
+        success, frame = cap.read()
+        #print(f"uspešnost: {success}, frejm: {frame}")
+        for code in decode(frame):
+            # print(code.data.decode("UTF8"))
+            txt = code.data.decode("UTF8")
+            x = txt.split("-")
+            id_, rn, slovo, sarza, naziv = x[0], x[1], x[2], x[3], x[4]
+            print(f"{id_=}")
+            print(f"{rn=}")
+            print(f"{slovo=}")
+            print(f"{sarza=}")
+            print(f"{naziv=}")
+            time.sleep(1)
+            if id_ != 0:
+                return id_, rn, slovo, sarza, naziv
+
+        cv2.imshow("Testing code scan", frame)
+        cv2.waitKey(20)
+
+    def scan_kvar_id():
+        cap = cv2.VideoCapture(0)
+        cap.set(3, 640) #3 - width
+        cap.set(4, 480) #4 - height
+
+        kvar_id, opis_kvara = 0, 0
+        print (kvar_id, opis_kvara)
+
+        camera = True # False by default ali kada se prtisne dugme za skenjranje, treba da promeni u True
+        while camera == True:
+            success, frame = cap.read()
+            #print(f"uspešnost: {success}, frejm: {frame}")
+            for code in decode(frame):
+                # print(code.data.decode("UTF8"))
+                txt = code.data.decode("UTF8")
+                x = txt.split("-")
+                kvar_id, opis_kvara = x[0], x[1]
+                print(f"{kvar_id=}")
+                print(f"{opis_kvara=}")
+                time.sleep(1)
+                if kvar_id != 0:
+                    return kvar_id, opis_kvara
+
+            cv2.imshow("Testing KVAR ID scan", frame)
+            cv2.waitKey(20)
